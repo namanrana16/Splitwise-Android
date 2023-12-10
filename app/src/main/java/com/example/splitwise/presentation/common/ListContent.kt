d1f9f89363788e62f7a2666d63d99d1041e61425
Naman Rana <namanrana1828@gmail.com>
2023-12-10 22:38:25 +0530

    Packages Setup

---
+++ app/src/main/java/com/example/splitwise/presentation/common/ListContent.kt
@@ -0,0 +1,64 @@
+package com.example.splitwise.presentation.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+import androidx.paging.compose.items
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+
+@ExperimentalCoilApi
+@Composable
+fun ListContent(navController: NavHostController) {
+}
+
+
+
+
+
+@Composable
+fun handlePagingResult() {
+
+
+}
+
+
+
+
+
+
+
+
+
+@ExperimentalCoilApi
+@Composable
+fun HeroItem(navController: NavHostController)
+{
+
+}
+
+
---
+++ app/src/main/java/com/example/splitwise/util/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.splitwise.util
+
+object Constants {
+    const val BASE_URL= ""
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+    const val BORUTO_DATABASE="boruto_database"
+    const val HERO_DATABASE_TABLE="hero_table"
+    const val HERO_REMOTE_KEYS_DATABASE_TABLE ="hero_remote_keys_table"
+    const val PREFERENCES_NAME = "boruto_preferences"
+    const val PREFERENCES_KEY = "onboarding_completed"
+    const val ITEMS_PER_PAGE = 3
+    const val USER_TABLE = "user_table"
+    const val STACKOVERFLOW_TABLE = "stackoverflow_table"
+}
